9c164c74b51702c785c0206b7bb651607f1dc6c8
Alicia Arver <alessarver@gmail.com>
2026-05-10 23:34:38 +0700

    chore: настроил деплой на gh-pages и подключил iconoir-react

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/ui-kit-animations/',
   plugins: [react()],
   css: {
     preprocessorOptions: {
