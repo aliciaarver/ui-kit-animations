462d39f6d5b7e4cab23272ea9254916e64c025dd
Alicia Arver <alessarver@gmail.com>
2026-05-11 00:48:29 +0700

    feat(T2OYsbLR): добавить index.ts для Typography с экспортом Vue-обёртки

---
+++ src/shared/ui/Typography/index.ts
@@ -0,0 +1,4 @@
+export { Typography } from './Typography';
+export type { TypographyProps, Variant, BaseTypographyProps } from './Typography';
+
+export { default as TypographyVue } from './Typography.vue';
